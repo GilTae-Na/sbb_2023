d1e74d1dc10b977076b6503a1da2e8d0a48aca07
nagt1997@gmail.com <nagt1997@gmail.com>
2023-03-20 16:46:01 +0900

    add service, dataException, question_detail

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@RequestMapping("/question") //URL 프리픽스(prefix), 공통 url 위로 뺴놓기, 필수는 아님
 @RequiredArgsConstructor //questionRepository 포함하는 생성자 생성, final이 붙은 속성을 포함하는 생성자를 자동으로 생성
 @Controller
 public class QuestionController {
@@ -15,11 +18,18 @@ public class QuestionController {
     private final QuestionService questionService;
     //questionService 객체는 생성자 방식으로 DI 규칙에 의해 주입된다.
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model){
 
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id){
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute(question);
+        return "question_detail";
     }
 }
